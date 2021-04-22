326163cf79c78ee8d8ca51d22d4d168fd159c0d3
Jake Ross <survivaldawn@gmail.com>
2021-04-22 17:57:14 -0500

    Update money_enjoyer.js

--- money_enjoyer.js
+++ money_enjoyer.js
@@ -1,3 +1,4 @@
+// All the DOM elements we need to latch on to for manipulation. 
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
@@ -5,46 +6,10 @@ const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
-
+// Data coming from the external API gets housed here. 
 let data = [];
 
-
-getRandomUser();
-getRandomUser();
-getRandomUser();
-getRandomUser();
-getRandomUser();
-
-
-async function getRandomUser() {
-    const res = await fetch('https://randomuser.me/api');
-    const data = await res.json();
-
-    const user = data.results[0];
-
-    const newUser = {
-        name: `${user.name.first} ${user.name.last}`, 
-        money: Math.floor(Math.random() * 2000000)
-    };
-
-    addData(newUser);
-}
-
-
-function doubleMoney() {
- data = data.map(user => {
-     return {...user, money: user.money * 2};
- })
- updateDOM();
-}
-
-
-function addData(obj) {
-    data.push(obj);
-    updateDOM();
-}
-
-
+// the function to take the JSON object data from the API and create formatted HTML elements out of it. 
 function updateDOM(providedData = data) {
     main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
     // for only one parameter, you dont need the extra surrounding parantheses. ((item) => {})
@@ -56,12 +21,61 @@ function updateDOM(providedData = data) {
     });
 }
 
-
-function formatMoney(number) {
-    // regex from stack over flow 
-    return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+// the function that handles that data from the API and pushes it into the DOM. 
+function addData(obj) {
+    data.push(obj);
+    updateDOM();
 }
+
+// the async await function that FETCHES our data/users. 
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    const user = data.results[0];
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`, 
+        money: Math.floor(Math.random() * 2000000)
+    };
+    // the function for this is declared above, on line 13
+    addData(newUser);
+}
+
+// pre-populating the page with 5 rich mother fuckers. 
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
 
 // event listeners for all the buttons 
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+
+// formatting the money correctly via REGEX
+function formatMoney(number) {
+    return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// prepping the functionality for when the double money button is clicked. map() method.  
+function doubleMoney() {
+ data = data.map(user => {
+     return {...user, money: user.money * 2};
+ })
+ updateDOM();
+}
+
+// prepping the functionality for when the sort by richest button is clicked. sort() method
+function sortByRichest() {
+    // since these are objects, we have to (b.money - a.money), we cannot just do (b - a)
+    data.sort((a, b) => b.money - a.money);
+    updateDOM();
+}
+
+// prepping the functionality for showing only millionaires. filter() method.
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+    updateDOM();
+}
